0cde71c5a3b4b3a3deaf06d5710d921c4e19eeed
Caique Daniel <caiquedaniel@outlook.com.br>
2024-02-13 00:44:16 -0300

    feat(Game): add entity and repository interface

---
+++ src/main/java/org/retrolauncher/app/games/domain/repositories/GameRepository.java
@@ -0,0 +1,7 @@
+package org.retrolauncher.app.games.domain.repositories;
+
+import org.retrolauncher.app._shared.domain.repository.Repository;
+import org.retrolauncher.app.games.domain.entities.Game;
+
+public interface GameRepository extends Repository<Game> {
+}
