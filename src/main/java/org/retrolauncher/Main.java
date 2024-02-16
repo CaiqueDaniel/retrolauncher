a25d6f722314b3a4d6b25a19fae199376478bdd8
Caique Daniel <caiquedaniel@outlook.com.br>
2024-02-16 01:50:08 -0300

    feat(Games): add list games cli command

--- src/main/java/org/retrolauncher/Main.java
+++ src/main/java/org/retrolauncher/Main.java
@@ -1,6 +1,7 @@
 package org.retrolauncher;
 
 import org.retrolauncher.app._shared.application.services.EnvConfigService;
+import org.retrolauncher.config.CommandsHandler;
 
 public class Main {
     private static final DependencyInjector dependencyInjector = new DependencyInjector();
@@ -8,14 +9,20 @@ public class Main {
     public static void main(String[] args) {
         try {
             Main.startup();
+            Main.registerCommands(args);
         } catch (Exception exception) {
-            throw new RuntimeException(exception);
+            System.out.println(exception.getMessage());
         }
     }
 
-    public static void startup() throws Exception {
+    private static void startup() throws Exception {
         EnvConfigService configService = Main.dependencyInjector.getConfigService();
+        System.out.println(configService.getCoresPath());
         Main.dependencyInjector.getUpdatePlatformsListUseCase().execute(configService.getCoresPath());
         Main.dependencyInjector.getUpdateGamesListUseCase().execute(configService.getRomsPath());
+    }
+
+    private static void registerCommands(String[] args) {
+        new CommandsHandler(Main.dependencyInjector, args).run();
     }
 }
---
+++ src/main/java/org/retrolauncher/config/CommandsHandler.java
@@ -0,0 +1,37 @@
+package org.retrolauncher.config;
+
+import org.retrolauncher.DependencyInjector;
+import org.retrolauncher.app.games.infrastructure.cli.commands.ListGamesCommand;
+import picocli.CommandLine;
+
+import java.util.*;
+
+public class CommandsHandler implements Runnable {
+    private final Deque<String> args;
+    private final List<CommandLine> commands;
+
+    public CommandsHandler(DependencyInjector dependencyInjector, String[] args) {
+        this.args = new LinkedList<>(List.of(args));
+        this.commands = List.of(
+                new CommandLine(new ListGamesCommand(dependencyInjector.getListGamesUseCase()))
+        );
+    }
+
+    @Override
+    public void run() {
+        if (this.args.size() == 0)
+            return;
+
+        String commandName = this.args.removeFirst();
+        Optional<CommandLine> foundCommand = this.commands.stream()
+                .filter((command) -> command
+                        .getCommandName()
+                        .equals(commandName))
+                .findFirst();
+
+        if (foundCommand.isEmpty())
+            return;
+
+        foundCommand.get().execute(this.args.toArray(String[]::new));
+    }
+}
