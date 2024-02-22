1c3febb333a69c0c864237369d6269643df3c94a
Caique Daniel <caiquedaniel@outlook.com.br>
2024-02-22 01:06:08 -0300

    feat(Game): add CreateGameShortcutUseCaseIntegrationTests

---
+++ src/test/java/integration/CreateGameShortcutUseCaseIntegrationTests.java
@@ -0,0 +1,56 @@
+package integration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.retrolauncher.app._shared.application.dtos.Shortcut;
+import org.retrolauncher.app._shared.application.services.ShortcutService;
+import org.retrolauncher.app.games.application.exceptions.GameNotFoundException;
+import org.retrolauncher.app.games.application.usecases.CreateGameShortcutUseCase;
+import org.retrolauncher.app.games.domain.entities.Game;
+import org.retrolauncher.app.games.domain.repositories.GameRepository;
+import org.retrolauncher.app.games.infrastructure.database.jackson.repositories.MemoryGameRepository;
+import org.retrolauncher.app.platforms.domain.entities.Platform;
+import org.retrolauncher.app.platforms.domain.repositories.PlatformRepository;
+import org.retrolauncher.app.platforms.infrastructure.database.jackson.repositories.MemoryPlatformRepository;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CreateGameShortcutUseCaseIntegrationTests {
+    private final PlatformRepository platformRepository = new MemoryPlatformRepository();
+    private final GameRepository repository = new MemoryGameRepository(this.platformRepository);
+    private final ShortcutService shortcutService = mock(ShortcutService.class);
+    private final Platform platform = new Platform("Test", "test", List.of("test"));
+    private final CreateGameShortcutUseCase sut = new CreateGameShortcutUseCase(repository, shortcutService);
+
+    @BeforeAll
+    public void beforeAll() {
+        platformRepository.save(platform);
+    }
+
+    @AfterEach
+    void afterEach() {
+        repository.clear();
+    }
+
+    @Test
+    void it_should_create_a_shortcut() throws IOException {
+        Game game = new Game("test", "testpath", "icon.png", platform);
+        repository.save(game);
+
+        sut.execute(game.getId().toString());
+        verify(shortcutService, times(1)).create(any(Shortcut.class));
+    }
+
+    @Test
+    void it_should_not_create_a_shortcut_when_game_does_not_exist() {
+        assertThrows(GameNotFoundException.class, () -> sut.execute(UUID.randomUUID().toString()));
+    }
+}
