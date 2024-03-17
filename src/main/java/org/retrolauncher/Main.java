5ec02bf6bf784d0ecd071bc50ba269a6cf53e02c
Caique Daniel <caiquedaniel@outlook.com.br>
2024-03-17 03:44:22 -0300

    feat: disable UI startup when using CLI

--- src/main/java/org/retrolauncher/Main.java
+++ src/main/java/org/retrolauncher/Main.java
@@ -2,14 +2,13 @@ package org.retrolauncher;
 
 import javafx.application.Application;
 import org.retrolauncher.backend.Backend;
-import org.retrolauncher.backend.config.DependencyInjector;
 import org.retrolauncher.gui.GUI;
 
 public class Main {
-    private static final DependencyInjector dependencyInjector = new DependencyInjector();
-
     public static void main(String[] args) {
         Backend.main(args);
-        Application.launch(GUI.class);
+
+        if (args.length == 0)
+            Application.launch(GUI.class);
     }
 }
