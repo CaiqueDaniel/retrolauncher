85eff46d34b7cfd31d7839d0e5ddba725e474040
Caique Daniel <caiquedaniel@outlook.com.br>
2024-04-10 23:48:20 -0300

    feat(Setting): add get method on SettingFacadeImpl

--- src/main/java/org/retrolauncher/backend/facades/SettingFacade.java
+++ src/main/java/org/retrolauncher/backend/facades/SettingFacade.java
@@ -1,7 +1,10 @@
 package org.retrolauncher.backend.facades;
 
 import org.retrolauncher.backend.app.settings.infrastructure.desktop.dtos.SaveSettingsDto;
+import org.retrolauncher.backend.app.settings.infrastructure.desktop.dtos.SettingsOutputDto;
 
 public interface SettingFacade {
     void save(SaveSettingsDto dto);
+
+    SettingsOutputDto get();
 }
--- src/main/java/org/retrolauncher/backend/facades/SettingFacadeImpl.java
+++ src/main/java/org/retrolauncher/backend/facades/SettingFacadeImpl.java
@@ -2,10 +2,16 @@ package org.retrolauncher.backend.facades;
 
 import org.retrolauncher.backend.Backend;
 import org.retrolauncher.backend.app.settings.infrastructure.desktop.dtos.SaveSettingsDto;
+import org.retrolauncher.backend.app.settings.infrastructure.desktop.dtos.SettingsOutputDto;
 
 public class SettingFacadeImpl implements SettingFacade {
     @Override
     public void save(SaveSettingsDto dto) {
         Backend.getDependencies().getSettingController().save(dto);
+    }
+
+    @Override
+    public SettingsOutputDto get() {
+        return Backend.getDependencies().getSettingController().get();
     }
 }
