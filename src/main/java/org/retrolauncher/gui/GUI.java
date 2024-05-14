bed30ad7286d634982533a37bd2cd3a03364edf3
Caique Daniel <caiquedaniel@outlook.com.br>
2024-05-14 01:00:18 -0300

    feat: add icon

--- src/main/java/org/retrolauncher/gui/GUI.java
+++ src/main/java/org/retrolauncher/gui/GUI.java
@@ -1,7 +1,11 @@
 package org.retrolauncher.gui;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.retrolauncher.Main;
+
+import java.util.Objects;
 
 public class GUI extends Application {
 
@@ -10,6 +14,7 @@ public class GUI extends Application {
         Routes.initialize(stage);
         Routes.getInstance().switchToHome();
 
+        stage.getIcons().add(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("assets/icon.png"))));
         stage.setTitle("Retro Launcher");
         stage.show();
     }
