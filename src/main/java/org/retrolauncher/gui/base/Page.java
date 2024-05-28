015c3ae0f58e6325d80e26c2593d2db4ab984a2f
Caique Daniel <caiquedaniel@outlook.com.br>
2024-05-27 23:13:50 -0300

    refactor: remove specific GamesPage createScene method

--- src/main/java/org/retrolauncher/gui/base/Page.java
+++ src/main/java/org/retrolauncher/gui/base/Page.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import org.retrolauncher.Main;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public abstract class Page {
     private final String template;
@@ -16,6 +17,11 @@ public abstract class Page {
     public Scene createScene() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(template));
         fxmlLoader.setController(this);
-        return new Scene(fxmlLoader.load());
+
+        Scene scene = new Scene(fxmlLoader.load());
+        String style = Objects.requireNonNull(Main.class.getResource("styles/button.css")).toExternalForm();
+        scene.getStylesheets().add(style);
+
+        return scene;
     }
 }
--- src/main/java/org/retrolauncher/gui/modules/games/pages/GamesPage.java
+++ src/main/java/org/retrolauncher/gui/modules/games/pages/GamesPage.java
@@ -1,22 +1,9 @@
 package org.retrolauncher.gui.modules.games.pages;
 
-import javafx.scene.Scene;
-import org.retrolauncher.Main;
 import org.retrolauncher.gui.base.Page;
-
-import java.io.IOException;
-import java.util.Objects;
 
 public class GamesPage extends Page {
     public GamesPage() {
         super("modules/games/pages/GamesPage.fxml");
-    }
-
-    @Override
-    public Scene createScene() throws IOException {
-        Scene scene = super.createScene();
-        String style = Objects.requireNonNull(Main.class.getResource("styles/button.css")).toExternalForm();
-        scene.getStylesheets().add(style);
-        return scene;
     }
 }
