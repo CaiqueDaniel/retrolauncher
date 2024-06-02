3a2ec203475214ce0fad99db4609878cdc31ac82
Caique Daniel <caiquedaniel@outlook.com.br>
2024-06-02 01:58:17 -0300

    feat(Game): update styles

--- src/main/java/org/retrolauncher/gui/modules/games/pages/GamesPage.java
+++ src/main/java/org/retrolauncher/gui/modules/games/pages/GamesPage.java
@@ -1,9 +1,21 @@
 package org.retrolauncher.gui.modules.games.pages;
 
+import javafx.scene.Scene;
+import org.retrolauncher.Main;
 import org.retrolauncher.gui.base.Page;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class GamesPage extends Page {
     public GamesPage() {
         super("modules/games/pages/GamesPage.fxml");
+    }
+
+    public Scene createScene() throws IOException {
+        final Scene scene = super.createScene();
+        final String style = Objects.requireNonNull(Main.class.getResource("styles/list-games.css")).toExternalForm();
+        scene.getStylesheets().add(style);
+        return scene;
     }
 }
