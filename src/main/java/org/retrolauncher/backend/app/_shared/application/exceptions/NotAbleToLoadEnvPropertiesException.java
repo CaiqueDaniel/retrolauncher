e4443c54fea68bbda322dfa0bf5ef2e26efd8b57
Caique Daniel <caiquedaniel@outlook.com.br>
2024-07-19 21:30:15 -0300

    refactor(Games): remove unused files

--- src/main/java/org/retrolauncher/backend/app/_shared/application/exceptions/NotAbleToLoadEnvPropertiesException.java
+++
@@ -1,9 +0,0 @@
-package org.retrolauncher.backend.app._shared.application.exceptions;
-
-public class NotAbleToLoadEnvPropertiesException extends RuntimeException {
-    private static final String MESSAGE = "Unable to load env file";
-
-    public NotAbleToLoadEnvPropertiesException(Exception cause) {
-        super(MESSAGE, cause);
-    }
-}
