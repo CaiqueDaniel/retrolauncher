8d1daa905609630e4bf28061ff20d2069c83fb5a
Caique Daniel <caiquedaniel@outlook.com.br>
2025-07-26 21:28:17 -0300

    refactor: move platform's factories

--- backend/tests/app/platform/application/create_platform_test.go
+++ backend/tests/app/platform/application/create_platform_test.go
@@ -2,8 +2,8 @@ package platform_application_test
 
 import (
 	platform_application "retrolauncher/backend/internal/app/platform/application"
+	platform_factories "retrolauncher/backend/internal/app/platform/factories"
 	platform_doubles_test "retrolauncher/backend/tests/app/platform/doubles"
-	platform_factories "retrolauncher/backend/tests/app/platform/factories"
 	"testing"
 )
 
