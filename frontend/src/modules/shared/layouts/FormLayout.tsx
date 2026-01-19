84fe2a11a4207fa5251761a287723299c3372af5
CaiqueDaniel <caiquedaniel@outlook.com.br>
2026-01-18 23:18:02 -0300

    fix: fix layout padding

--- frontend/src/modules/shared/layouts/FormLayout.tsx
+++ frontend/src/modules/shared/layouts/FormLayout.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from "react";
 export function FormLayout({ children }: PropsWithChildren) {
     return (
         <Box bgcolor="background.default" height="100vh" width="100vw">
-            <Box p={3} display="flex" justifyContent="center" alignItems="center" height="100%">
+            <Box display="flex" justifyContent="center" alignItems="center" height="100%">
                 <Paper sx={{ p: 3, mb: 3, maxWidth: 800 }}>
                     {children}
                 </Paper>
